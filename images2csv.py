0b205f97ad9458e8bea459ab376d30f8ce629862
suchy1713 <wawrzynow.piotr@gmail.com>
2020-03-04 19:31:35 +0100

    fixes

--- images2csv.py
+++ images2csv.py
@@ -4,6 +4,7 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 import sys
+import csv
 
 directory = os.fsencode('images/cats')
 
@@ -13,10 +14,15 @@ y = []
 for file in glob.iglob('images/cats/*'):
     print(file)
     image = Image.open(file).convert('RGB')
-    X.append(np.asarray(image))
-    y.append(file.split('/')[2].split('_')[0])
+    X.append(np.asarray(image).tolist())
+    filename = file.split('/')[2].split('_')
+
+    if len(filename) == 2:
+        y.append(filename[0])
+    else:
+        y.append(filename[0] + ' ' + filename[1])
 
 df = pd.DataFrame(data={'X': X, 'y': y})
 
 np.set_printoptions(threshold=sys.maxsize)
-df.to_csv('data/cats.csv', index=False, sep='\t')
+df.to_csv('data/cats.csv', index=False, quotechar='"', encoding='ascii')
